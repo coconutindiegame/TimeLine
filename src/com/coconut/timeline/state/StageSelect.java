96f4e45f0d672164f30b54e7e5f12a49c26354de
nayoungwook <nayoungwoook@gmail.com>
2022-08-03 00:29:10 +0900

    0803 done :D

---
+++ src/com/coconut/timeline/state/StageSelect.java
@@ -0,0 +1,22 @@
+package com.coconut.timeline.state;
+
+import dev.suback.marshmallow.state.MSState;
+
+public class StageSelect implements MSState {
+
+	@Override
+	public void init() {
+
+	}
+
+	@Override
+	public void render() {
+
+	}
+
+	@Override
+	public void update() {
+
+	}
+
+}
